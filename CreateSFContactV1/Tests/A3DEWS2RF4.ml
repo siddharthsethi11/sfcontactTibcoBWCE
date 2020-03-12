00f780ff2b4e2673791429d4e53dbc9964ddb8b9
Cris <cristobal.madrigal@gmail.com>
2020-03-12 12:17:21 -0400

    commit v1

---
+++ CreateSFContactV1/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/CreateSFContactV1/Tests/CreateSFContactV12.bwt#/"/>
+  <EmulationDataFile>ACED00057400164372656174655346436F6E746163745631312E627774</EmulationDataFile>
+  <EmulationDataFile>ACED00057400164372656174655346436F6E746163745631322E627774</EmulationDataFile>
+</emulation:BWTFile>
